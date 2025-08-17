15a5d020196262f318840baeb7194c9305e17343
Puffx Host <nitinmehta368@gmail.com>
2025-08-17 13:05:32 +0530

    Update hooks.php

--- modules/addons/hooks.php
+++ modules/addons/hooks.php
@@ -1,12 +1,19 @@
 <?php
-if (!defined("WHMCS"))
+if (!defined("WHMCS")) {
     die("This file cannot be accessed directly");
+}
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-add_hook('ClientAreaHeadOutput', 1, function($vars) {
-    if (\WHMCS\Config\Setting::getValue('FKMBCSTATUS')) {
-        return '<meta name="msapplication-TileColor" content="' . \WHMCS\Config\Setting::getValue('FKMBCCOLOR') . '">
-    <meta name="theme-color" content="' . \WHMCS\Config\Setting::getValue('FKMBCCOLOR') . '">';
+/**
+ * Puffx Host - Mobile Browser Tab Color Hook
+ * Adds <meta name="theme-color"> in client area
+ */
+
+add_hook('ClientAreaHeadOutput', 1, function ($vars) {
+    if (\WHMCS\Config\Setting::getValue('PUFFX_MBC_STATUS')) {
+        $color = \WHMCS\Config\Setting::getValue('PUFFX_MBC_COLOR');
+        return '<meta name="msapplication-TileColor" content="' . $color . '">
+                <meta name="theme-color" content="' . $color . '">';
     }
 });
